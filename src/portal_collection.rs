85bdc7b76195059a76e0f32bee91da214c029a5f
MouseProducedGames <largemouse@gmail.com>
2020-10-08 12:49:23 -0700

    Add: PortalCollection and SubRoomCollection.

---
+++ src/portal_collection.rs
@@ -0,0 +1,17 @@
+// External includes.
+
+// Standard includes.
+
+// Internal includes.
+use super::{PlacedRoom, Portal};
+use crate::geometry::LocalPosition;
+
+pub trait PortalCollection<'a> {
+    fn add_portal(&mut self, local: LocalPosition, target: &dyn PlacedRoom<'a>);
+
+    fn get_portal_at(&self, index: usize) -> Option<&Portal<'a>>;
+
+    fn get_portal_at_mut(&mut self, index: usize) -> Option<&mut Portal<'a>>;
+
+    fn portal_count(&self) -> usize;
+}
---
+++ src/sub_room_collection.rs
@@ -0,0 +1,17 @@
+// External includes.
+
+// Standard includes.
+
+// Internal includes.
+use super::{Room, SubRoom};
+use crate::geometry::LocalPosition;
+
+pub trait SubRoomCollection<'a> {
+    fn add_sub_room(&mut self, local: LocalPosition, target: &dyn Room<'a>);
+
+    fn get_sub_room_at(&self, index: usize) -> Option<&SubRoom<'a>>;
+
+    fn get_sub_room_at_mut(&mut self, index: usize) -> Option<&mut SubRoom<'a>>;
+
+    fn sub_room_count(&self) -> usize;
+}
