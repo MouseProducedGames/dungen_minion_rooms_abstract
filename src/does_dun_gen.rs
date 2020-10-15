f64a126537c2d18013073f9d821daaa34de266cb
MouseProducedGames <largemouse@gmail.com>
2020-10-15 10:33:37 -0700

    Change: Default implementation for DoesDunGen.dun_gen(); modified documentation in DoesDunGen.

--- src/does_dun_gen.rs
+++ src/does_dun_gen.rs
@@ -7,9 +7,12 @@ use super::{MapId, SupportsDunGen};
 
 /// A trait for types that enact some aspect of dungeon generation on a [`Map`](trait.Map.html).
 pub trait DoesDunGen {
-    /// Accepts any type that implements `SupportsDunGen`. Implementations should generally then extract the `MapId` and defer to dun_gen_map.
-    fn dun_gen(&self, target: &mut dyn SupportsDunGen);
+    /// Accepts any type that implements `SupportsDunGen`. The default implementation is to extract the `MapId` and defer to dun_gen_map.
+    fn dun_gen(&self, target: &mut dyn SupportsDunGen) {
+        let map_id = target.get_map_id();
+        self.dun_gen_map(map_id)
+    }
 
-    /// Modifies the map for the supplied 'MapId' using some formula or method.
+    /// Modifies the map for the supplied 'MapId'. See the documentation for specific implementations for details.
     fn dun_gen_map(&self, map_id: MapId);
 }
